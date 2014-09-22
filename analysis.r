401c5a8589865288b358251e20143e2bebb83f0c
Willi <willi@jups42.de>
2014-09-22 01:14:24 -0300

    normalizing works, completing nearly

--- analysis.r
+++ analysis.r
@@ -1,3 +1,5 @@
+library(sqldf)
+
 readUsers <- function(path="base2/arabiaSaudita/profilesArabia.dat") {
   fullPath <- paste("~/studium/Lehrveranstaltungen/informationRetrieval/GenderSocialMedia/datasets/", path, sep="")
   saU <- read.csv(fullPath, header=F, sep="\t")
@@ -30,11 +32,38 @@ categoriesByGender <- function(table, gender, uniqueUsers=FALSE, subcategory=FAL
   queryString <- paste("Select *, count(*) as count from ", table,
                      " where gender='", gender, "' group by ", category, sep="")
   if(uniqueUsers==TRUE) {
-    queryString <- paste("Select *, count(*) as CCount from  (select * from ", table,
+    queryString <- paste("Select *, count(*) as count from  (select * from ", table,
                       " where gender='", gender, "' group by user, ",
                        category, " ) group by ",  category, sep="")
   }
   return(sqldf(queryString))
+}
+
+normalizeByAbsolutePercentage <- function(attribute){
+  return(attribute/sum(attribute))
+}
+
+normalizeByPercentageOfMax <- function(attribute) {
+  return(attribute/max(attribute))
+}
+
+correlateCategories <- function(x, y, labels, countMethod="check-ins", categories="Categories") {
+  plot(x, y, main=paste("Correlation of", categories, "counting", countMethod), xlab="Male", ylab="Female")
+  abline(0, 1, col="red")
+  text(x, y, labels=labels, pos=3)
+  cor.test(x, y)
+  chisq.test(x, y)
+}
+
+selectNotPresentFromOtherGender <- function(x, y) {
+  return(sqldf(paste("Select * from", x, "where subcategory not in (Select subcategory from", y,")")))
+}
+
+completeSubcategories <- function(gender1, gender2, gender1String, gender2String) {
+  temp <- selectNotPresentFromOtherGender(gender1String, gender2String)
+  temp$count <- 0
+  gender2<-rbind(gender2, temp)
+  return( gender2[ order(gender2$subcategory), ] )
 }
 
 
@@ -46,90 +75,61 @@ sa <- readCheckIns()
 cSaU <- cleanUsers(readUsers())
 
 joined <- joinCheckInsWithProfiles("cSaU", "sa")
-tableString <- deparse(substitute(joined))
+tableString <- "joined"
 saMC <- categoriesByGender(tableString, "male")
 saFC <- categoriesByGender(tableString, "female")
 saUMC <- categoriesByGender(tableString, "male", uniqueUsers=TRUE)
-saFMC <- categoriesByGender(tableString, "female", uniqueUsers=TRUE)
+saUFC <- categoriesByGender(tableString, "female", uniqueUsers=TRUE)
 
 saMS <- categoriesByGender(tableString, "male", subcategory=TRUE)
 saFS <- categoriesByGender(tableString, "female", subcategory=TRUE)
 saUMS <- categoriesByGender(tableString, "male", subcategory=TRUE, uniqueUsers=TRUE)
 saUFS <- categoriesByGender(tableString, "female", subcategory=TRUE, uniqueUsers=TRUE)
 
-
 ## normalization
-saMC$count <- saMC$count/sum(saMC$count)
-saFC$count <- saFC$count/sum(saFC$count)
-
-saMS$count <- saMS$count/sum(saMS$count)
-saFS$count <- saFS$count/sum(saFS$count)
+saMC$count <- normalizeByAbsolutePercentage(saMC$count)
+saFC$count <- normalizeByAbsolutePercentage(saFC$count)
+saMS$count <- normalizeByAbsolutePercentage(saMS$count)
+saFS$count <- normalizeByAbsolutePercentage(saFS$count)
 
 ### normalization – unique users
-saUMC$CCount <- saUMC$CCount/sum(saUMC$CCount)
-saUFC$CCount <- saUFC$CCount/sum(saUFC$CCount)
-
-saUMS$CCount <- saUMS$CCount/sum(saUMS$CCount)
-saUFS$CCount <- saUFS$CCount/sum(saUFS$CCount)
+saUMC$count <- normalizeByAbsolutePercentage(saUMC$count)
+saUFC$count <- normalizeByAbsolutePercentage(saUFC$count)
+saUMS$count <- normalizeByAbsolutePercentage(saUMS$count)
+saUFS$count <- normalizeByAbsolutePercentage(saUFS$count)
 
 ## correlation categories
-### counting check-ins
-plot(saMC$count, saFC$count, main="Correlation Categories counting check-ins", xlab="Male", ylab="Female")
-abline(0, 1, col="red")
-text(saMC$count, saFC$count, labels=saMC$category, pos=3)
-cor.test(saFC$count, saMC$count)
+correlateCategories(saMC$count, saFC$count, saMC$category)
+correlateCategories(saUMC$count, saUFC$count, saMC$category, countMethod="unique users")
 
-### counting check-ins by unique-users (counting every user once)
-plot(saUMC$CCount, saUFC$CCount, main="Correlation Categories counting unique users", xlab="Male", ylab="Female")
-abline(0, 1, col="red")
-text(saUMC$CCount, saUFC$CCount, labels=saUMC$category, pos=3)
-cor.test(saUMC$CCount, saUFC$CCount)
+saMS <- completeSubcategories(saMS, saFS, "saMS", "saFS")
+saFS <- completeSubcategories(saFS, saMS, "saFS", "saMS")
+saUMS <- completeSubcategories(saUMS, saUFS, "saUMS", "saUFS")
+saUFS <- completeSubcategories(saUFS, saUMS, "saUFS", "saUMS")
 
-chisq.test(saUMC$CCount, saUFC$CCount)
+stopifnot(length(saUFS$count) == length(saUMS$count))
 
-## complete subcategories
-temp <- sqldf("Select * from saMS where subcategory not in (Select subcategory from saFS)")
-temp$count=0
-saFS <- rbind(saFS, temp)
-
-temp <- sqldf("Select * from saFS where subcategory not in (Select subcategory from saMS)")
-temp$count=0
-saMS <- rbind(saMS, temp)
-
-saFS <- saFS[ order(saFS$subcategory), ]
-saMS <- saMS[ order(saMS$subcategory), ]
-
-### with unique users
-temp <- sqldf("Select * from saUMS where subcategory not in (Select subcategory from saUFS)")
-temp$CCount=0
-saUFS <- rbind(saUFS, temp)
-
-temp <- sqldf("Select * from saUFS where subcategory not in (Select subcategory from saUMS)")
-temp$CCount=0
-saUMS <- rbind(saUMS, temp)
-
-saUFS <- saUFS[ order(saUFS$subcategory), ]
-saUMS <- saUMS[ order(saUMS$subcategory), ]
+correlateCategories(saMS$count, saFS$count, saMS$subcategory, categories="Subcategories")
+correlateCategories(saUMS$count, saUFS$count, saUMS$subcategory, categories="Subcategories")
 
 ## normalize subcategories
 saFSR <- saFS; saMSR <- saMS
-saFSR$count <- saFSR$count/sum(saFSR$count)
-saMSR$count <- saMSR$count/sum(saMSR$count)
+saFSR$count <- normalizeByAbsolutePercentage(saFSR$count)
+saMSR$count <- normalizeByAbsolutePercentage(saMSR$count)
 
-### with unique users
 saUFSR <- saUFS; saUMSR <- saUMS
-saUMSR$CCount <- saUMSR$CCount/sum(saUMSR$CCount)
-saUFSR$CCount <- saUFSR$CCount/sum(saUFSR$CCount)
+saUMSR$count <- normalizeByAbsolutePercentage(saUMSR$count)
+saUFSR$count <- normalizeByAbsolutePercentage(saUFSR$count)
 
-## correlate subcategories
-plot(saMSR$count, saFSR$count, main="Correlation Subcategories by Gender, Counting check-ins", xlab="Male", ylab="Female")
-abline(0,1, col="red")
-text(saMSR$count, saFSR$count, labels=saMSR$subcategory, pos=3)
-cor.test(saMC$count, saFC$count)
+## test
+stopifnot(length(saUFSR$count) == length(saUMSR$count))
 
+saMSR <- completeSubcategories(saMSR, saFSR, "saMSR", "saFSR")
+saFSR <- completeSubcategories(saFSR, saMSR, "saFSR", "saMSR")
 
-### with unique users
-plot(saUMSR$CCount, saUFSR$CCount, main="Correlation Subcategories by Gender, Counting Unique Users", xlab="Male", ylab="Female")
-abline(0,1, col="red")
-text(saUMSR$CCount, saUFSR$CCount, labels=saUMSR$subcategory, pos=3)
-cor.test(saUMSR$CCount, saUFSR$CCount)
+saUMSR <- completeSubcategories(saUMSR, saUFSR, "saUMSR", "saUFSR")
+saUFSR <- completeSubcategories(saUFSR, saUMSR, "saUFSR", "saUMSR")
+
+correlateCategories(saMSR$count, saFSR$count, saMSR$subcategory, categories="Subcategories")
+correlateCategories(saUMSR$count, saUFSR$count, saUMSR$subcategory, categories="Subcategories",
+                    countMethod="uniqueUsers")
