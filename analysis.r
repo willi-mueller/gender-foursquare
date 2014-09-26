acbcc4ed21fc1a05a72f70df6ce09327a8c43737
Willi <willi@jups42.de>
2014-09-26 17:37:08 -0300

    aggregate multiplex, (indie) movie theater

--- analysis.r
+++ analysis.r
@@ -118,6 +118,7 @@ substitutionRules <- list(
     list(original="Train Station", equivalents=c("Train", "Platform", "Light Rail")),
     list(original="Bus Station", equivalents=c("Bus Line", "Bus Terminal")),
     list(original="Library", equivalents=c("College Library")),
+    list(original="Movie Theater", equivalents=c("Indie Movie Theater", "Multiplex")),
     list(original="University", equivalents=c("General College & University", "College & University")),
     list(original="Gym", equivalents=c("Gym / Fitness Center", "College Gym")))
 
