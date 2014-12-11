cdf31beb9eaec171dafbc6f76e0087d741966a4b
Willi <willi@jups42.de>
2014-12-11 17:23:00 -0200

    fix

--- category-stats.r
+++ category-stats.r
@@ -26,27 +26,28 @@ generateNullModel <- function() {
 		country <- strsplit(country, ".", fixed=T)[[1]][[1]] # remove .dat
 		message(country)
 		ci <- try(fread(f, header=F, sep="\t", stringsAsFactors=FALSE))
-		if(nrow(ci) < THRESH) {
-			message("<", THRESH, "check-ins")
-		}
 		if(length(ci)>2) {
-			setnames(ci, 1:12, c("idUserFoursquare", "date", "latitude", "longitude", "idLocal",
-                      "subcategory", "category", "country", "city", "district", "gender", "timeOffset"))
+			if(nrow(ci) < THRESH) {
+				message("<", THRESH, "check-ins")
+			} else {
+				setnames(ci, 1:12, c("idUserFoursquare", "date", "latitude", "longitude", "idLocal",
+	                      "subcategory", "category", "country", "city", "district", "gender", "timeOffset"))
 
-			ci <- ci[gender== "male" | gender=="female", ]
-			ci<- oneCheckInForUserAndLocation(ci)
-			ci <- checkInsInlocationsWithMinimumCheckIns(ci, n=5)
-			if(nrow(ci) > 0) {
-				gen.segregation <- runPermutate(ci, sprintf("results/null-model/%s/gender-permutation", country),
-	                                  "permutate-gender", country, k=k)
-				ci.segregation <- segregation(ci, country, log=F)
+				ci <- ci[gender== "male" | gender=="female", ]
+				ci<- oneCheckInForUserAndLocation(ci)
+				ci <- checkInsInlocationsWithMinimumCheckIns(ci, n=5)
+				if(nrow(ci) > 0) {
+					gen.segregation <- runPermutate(ci, sprintf("results/null-model/%s/gender-permutation", country),
+		                                  "permutate-gender", country, k=k)
+					ci.segregation <- segregation(ci, country, log=F)
 
-				folderName <- sprintf("results/null-model/%s/gender-permutation", country)
-				stats <- testObservationWithNullModelForCategories(ci.segregation,
-																			gen.segregation, folderName, country,
-	                                                    					k, quote(category))
-				print(stats)
-				categoryStats[[i]] <- stats
+					folderName <- sprintf("results/null-model/%s/gender-permutation", country)
+					stats <- testObservationWithNullModelForCategories(ci.segregation,
+																				gen.segregation, folderName, country,
+		                                                    					k, quote(category))
+					print(stats)
+					categoryStats[[i]] <- stats
+				}
 			}
 		}
 	}
