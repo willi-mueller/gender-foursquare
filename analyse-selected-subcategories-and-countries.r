afbc5ff128cd8f68e0f93f12117b1e18d77aa73c
Willi <willi@jups42.de>
2014-12-19 16:57:53 -0200

    refactoring + run for top3, top5, top10

--- analyse-selected-subcategories-and-countries.r
+++ analyse-selected-subcategories-and-countries.r
@@ -26,61 +26,73 @@ pickCIinTopLocations <- function(ci, topN) {
 	return(topCI)
 }
 
+AXES = c(0, 0.5)
 MAIN_FOLDER <- "results/null-model/selected-subcategories-and-countries"
 TOP_N <- 5
 k <- 1000
 
-countryFileNames <- c("Brazil", "United-States", "Indonesia", "Turkey", "Japan", "Saudi-Arabia", "Russia")
-allci <- rbindlist( mclapply(countryFileNames, function(x) {
-	readCheckIns(sprintf("paises/%s.dat", x))
-	}, mc.cores=N_CORES))
-countries <- c("Brazil", "United States", "Indonesia", "Turkey", "Japan", "Saudi Arabia", "Russia")
+run <- function(TOP_N, k) {
+	countryFileNames <- c("Brazil", "United-States", "Indonesia", "Turkey", "Japan", "Saudi-Arabia", "Russia")
 
-stopifnot( all(allci[, list(n=.N), by=idLocal]$n >= 5) )
+	allci <- rbindlist( mclapply(countryFileNames, function(x) {
+		readCheckIns(sprintf("paises/%s.dat", x))
+		}, mc.cores=N_CORES))
+	countries <- c("Brazil", "United States", "Indonesia", "Turkey", "Japan", "Saudi Arabia", "Russia")
 
-selectedCI <- allci[country %in% countries]
-subc <- Reduce(intersect, selectedCI[, .(list(unique(subcategory))), country]$V1)
-selectedCI <- selectedCI[subcategory %in% subc]
+	stopifnot( all(allci[, list(n=.N), by=idLocal]$n >= 5) )
 
-nCheckInsStats <- selectedCI[, list(n=length(idUserFoursquare)), by=list(country, idLocal, subcategory)][order(country, subcategory)]
-write.table(nCheckInsStats, sprintf("%s/n-checkIns-for-selected-subc-locations.csv", MAIN_FOLDER), row.names=F, sep="\t")
+	selectedCI <- allci[country %in% countries]
+	subc <- Reduce(intersect, selectedCI[, .(list(unique(subcategory))), country]$V1)
+	selectedCI <- selectedCI[subcategory %in% subc]
 
-nLocationsStats <- selectedCI[, list(nLocations=length(unique(idLocal))),by=list(subcategory, country)][order(subcategory, country)]
-write.table(nLocationsStats, sprintf("%s/n-locations-for-selected-subc.csv", MAIN_FOLDER), row.names=F, sep="\t")
+	nCheckInsStats <- selectedCI[, list(n=length(idUserFoursquare)), by=list(country, idLocal, subcategory)][order(subcategory, country)]
+	write.table(nCheckInsStats, sprintf("%s/n-checkIns-for-selected-subc-locations-top-%s.csv", MAIN_FOLDER, TOP_N), row.names=F, sep="\t")
 
-chosenSubcStats <- nLocationsStats[, .SD[all(nLocations>20)],by=subcategory]
-chosenSubc <- unique(chosenSubcStats$subcategory)
+	nLocationsStats <- selectedCI[, list(nLocations=length(unique(idLocal))),by=list(subcategory, country)][order(subcategory, country)]
+	write.table(nLocationsStats, sprintf("%s/n-locations-for-selected-subc-top%s.csv", MAIN_FOLDER, TOP_N), row.names=F, sep="\t")
 
-chosenSubcCI <- selectedCI[subcategory %in% chosenSubc]
-nrow(chosenSubcCI) #== 187851
+	chosenSubcStats <- nLocationsStats[, .SD[all(nLocations>20)],by=subcategory]
+	chosenSubc <- unique(chosenSubcStats$subcategory)
 
+	message("Chose subcategories: ", chosenSubc)
 
-topCI <- rbindlist( lapply(countries, function(countryStr) {
-												countryCI <- chosenSubcCI[country==countryStr]
-												pickCIinTopLocations(countryCI,  TOP_N)
-											}) )
-nrow(topCI) # = 14316
-
-genderDist <- topCI[,list(nCheckIns=.N), by=list(country, gender)]
-write.table(genderDist, sprintf("%s/gender-check-ins-for-selected-subcategories-and-countries.csv", MAIN_FOLDER)
+	chosenSubcCI <- selectedCI[subcategory %in% chosenSubc]
+	message("Analysing ", nrow(chosenSubcCI), " check-ins") #== 187851
 
 
-stats <- rbindlist( lapply( countries, function(countryStr){
-	folder <- sprintf("%s/%s", MAIN_FOLDER, countryStr)
-	dir.create(folder)
-	countryCI <- topCI[country==countryStr]
-	seg <- segregation(countryCI, countryStr, log=T)
-	gen <- 	runPermutate(countryCI, folder,
-				 sprintf("%s-selected-permutated", countryStr),
-				 countryStr,
-				 k=k, forceGenerate=T)
+	topCI <- lapply(countries, function(countryStr) {
+									countryCI <- chosenSubcCI[country==countryStr]
+									pickCIinTopLocations(countryCI,  TOP_N)
+								}) )
+	topCI <- rbindlist(topCI)
 
-	return( testObservationWithNullModel(seg, gen, folder , countryStr, k=k, PLOT_ANOM_DIST=T ) )
-}) )
+	genderDist <- topCI[,list(nCheckIns=.N), by=list(country, gender)]
+	write.table(genderDist, sprintf("%s/gender-check-ins-for-selected-subcategories-and-countries-top-%s.csv", MAIN_FOLDER, TOP_N))
+	message("In top locations ", nrow(topCI), " check-ins") # = 14316
+	print(genderDist)
 
-write.table(stats, sprintf("%s/selected-stats.csv",MAIN_FOLDER), sep="\t")
-nAnomalousPerCountry <- stats[, list(nAnomalous=length(isAnomalous[isAnomalous==T])), by=country][order(nAnomalous)]
-write.table(nAnomalousPerCountry, sprintf("%s/n-anomalous-per-country.csv",MAIN_FOLDER), sep="\t")
+	stats <- rbindlist( lapply( countries, function(countryStr){
+		folder <- sprintf("%s/%s", MAIN_FOLDER, countryStr)
+		dir.create(folder)
+		countryCI <- topCI[country==countryStr]
+		seg <- segregation(countryCI, countryStr, log=T, axeslim=AXES)
+		gen <- 	runPermutate(countryCI, folder,
+							 sprintf("%s-selected-permutated", countryStr),
+							 countryStr,
+							 k=k, forceGenerate=T)
+		permutationStats <- testObservationWithNullModel(seg, gen, folder , countryStr, k=k,
+														 PLOT_ANOM_DIST=T, axeslim=AXES)
+		return( permutationStats )
+	}) )
+
+	write.table(stats, sprintf("%s/selected-stats.csv",MAIN_FOLDER), sep="\t")
+	nAnomalousPerCountry <- stats[, list(nAnomalous=length(isAnomalous[isAnomalous==T])), by=country][order(nAnomalous)]
+	write.table(nAnomalousPerCountry, sprintf("%s/n-anomalous-per-country-select-top-%s-locations.csv",MAIN_FOLDER, TOP_N), sep="\t")
+	print(nAnomalousPerCountry)
+}
+
+for(n in c(3, 5, 10))
+	run(n, k)
 
 ######## TODO
 # Pub == Bar?
